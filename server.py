bc42607a63582934f39d9f20479ddbced05136f1
Vũ Hiền <hien037305@gmail.com>
2026-04-10 14:40:33 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,27 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+
+    text = request.args.get("textToAnalyze")
+
+    if not text or text.strip() == "":
+        return {
+            "error": "400",
+            "message": "Input cannot be empty"
+        }
+
+    result = emotion_detector(text)
+
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
