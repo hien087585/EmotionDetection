c213ffdc1257dad8ee7280381f6e200468503431
Vũ Hiền <hien037305@gmail.com>
2026-04-10 14:40:48 +0700

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,1 +1,20 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
 
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        result = emotion_detector("I am very happy and excited")
+        self.assertEqual(result["dominant_emotion"], "joy")
+
+    def test_sadness(self):
+        result = emotion_detector("I am very sad and depressed")
+        self.assertEqual(result["dominant_emotion"], "sadness")
+
+    def test_empty(self):
+        result = emotion_detector("")
+        self.assertEqual(result["error"], "400")
+
+
+if __name__ == "__main__":
+    unittest.main()
